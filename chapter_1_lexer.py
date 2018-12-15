33c6806a5182cff376edc8eb8f216a3fa693508c
Lewis <artoria@artoria.us>
2018-12-15 23:15:36 +0900

    shit

--- chapter_1_lexer.py
+++ chapter_1_lexer.py
@@ -1,44 +1,73 @@
 from enum import Enum
+from collections import namedtuple
 
 
-class Token(Enum):
+class TokenKind(Enum):
     EOF = -1
     DEF = -2
     EXTERN = -3
     IDENTIFIER = -4
-    NUM = -5
+    NUMBER = -5
     OPERATOR = -6
+
+
+Token = namedtuple('Token', 'kind value')
 
 
 class Lexer:
     def __init__(self, code: str):
         self.code: str = code
 
-    @staticmethod
-    def token_mapper(s):
-        if s == "def":
-            return (Token.DEF, s)
-        elif s == "extern":
-            return (Token.EXTERN, s)
-        elif s in  ("*", "+", "-", "/", "%", "^", "="):
-            return (Token.OPERATOR, s)
-        elif s[0].isdigit():
-            if not s.replace(".", "").isdigit():
-                raise SyntaxError("LEX: Identifier must be started with character")
-            if s.count(".") > 1:
-                raise SyntaxError("LEX: Not a valid number type")
-            s = float(s) if "." in s else int(s)
-            return (Token.NUM, s)
-        else:
-            return (Token.IDENTIFIER, s)
+        self.pos = 0
+        self.last_char = self.code[0]
 
-    def tokenizer(self):
-        code = [x for x in self.code.split() if x[0] != "#"]
-        tokenized_code = [self.token_mapper(x) for x in code]
-        tokenized_code.append((Token.EOF, "\n"))
+    def tokens(self):
+        while self.last_char:
+            # Skip whitespace
+            while self.last_char.isspace():
+                self.advance()
+            # Identifier or keyword
+            if self.last_char.isalpha():
+                id_str = ''
+                while self.last_char.isalnum():
+                    id_str += self.last_char
+                    self.advance()
+                if id_str == 'def':
+                    yield Token(kind=TokenKind.DEF, value=id_str)
+                elif id_str == 'extern':
+                    yield Token(kind=TokenKind.EXTERN, value=id_str)
+                else:
+                    yield Token(kind=TokenKind.IDENTIFIER, value=id_str)
+            # Number
+            elif self.last_char.isdigit() or self.last_char == '.':
+                num_str = ''
+                while self.last_char.isdigit() or self.last_char == '.':
+                    num_str += self.last_char
+                    self.advance()
+                yield Token(kind=TokenKind.NUMBER, value=num_str)
+            # Comment
+            elif self.last_char == '#':
+                self.advance()
+                while self.last_char and self.last_char not in '\r\n':
+                    self.advance()
+            elif self.last_char:
+                # Some other char
+                yield Token(kind=TokenKind.OPERATOR, value=self.last_char)
+                self.advance()
+        yield Token(kind=TokenKind.EOF, value='')
 
-        return tokenized_code
+    def advance(self):
+        try:
+            self.pos += 1
+            self.last_char = self.code[self.pos]
+        except IndexError:
+            self.last_char = ''
 
 
 if __name__ == "__main__":
-    print(Lexer("def extern 12423.3 + * am #asd code \n adsfasdf").tokenizer())
+    a = list(Lexer("def extern 12 + *am+ #asd code adsfasdf").tokens())
+    print(a)
+
+
+
+
