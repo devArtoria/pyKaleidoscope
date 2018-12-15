81d32ffcd9e07bc2c1d7e16a61a0a34cf2a43bae
Lewis <artoria@artoria.us>
2018-12-15 22:25:24 +0900

    update: operator lexing

--- chapter_1_lexer.py
+++ chapter_1_lexer.py
@@ -7,6 +7,7 @@ class Token(Enum):
     EXTERN = -3
     IDENTIFIER = -4
     NUM = -5
+    OPERATOR = -6
 
 
 class Lexer:
@@ -19,6 +20,8 @@ class Lexer:
             return (Token.DEF, s)
         elif s == "extern":
             return (Token.EXTERN, s)
+        elif s in  ("*", "+", "-", "/", "%", "^", "="):
+            return (Token.OPERATOR, s)
         elif s[0].isdigit():
             if not s.replace(".", "").isdigit():
                 raise SyntaxError("LEX: Identifier must be started with character")
@@ -38,4 +41,4 @@ class Lexer:
 
 
 if __name__ == "__main__":
-    print(Lexer("def extern 12423.3 am #asd code \n adsfasdf").tokenizer())
+    print(Lexer("def extern 12423.3 + * am #asd code \n adsfasdf").tokenizer())
